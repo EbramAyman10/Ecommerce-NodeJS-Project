ea5b7dd46755fa42d9893adc978fd9c96a2ca9a7
Ebram Ayman <ebramayman55@gmail.com>
2026-08-11 18:31:08 +0300

    add webhook

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ import { dbConnection } from "./database/dbConnection.js";
 import { globalError } from "./src/middleware/globalError.js";
 import { AppError } from "./src/utils/appError.js";
 import { bootstrap } from "./src/modules/bootstrap.routes.js";
+import catchError from "./src/middleware/catchError.js";
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  catchError((req, res) => {
+    const signature = req.headers["stripe-signature"];
+    let event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      "whsec_0MBVjEv0jqfjm4sadSag7JREecJd6R3y",
+    );
+
+    let checkout;
+    if (event.type === "checkout.session.completed") {
+      checkout = event.data.object;
+    }
+    res.json({ message: "success", checkout });
+  }),
+);
 
 app.use(cors());
 app.set("query parser", "extended");
